8f9689b399cea89905bd727eb5f475f393486823
nikhil chouksey <nikhilranger18@gmail.com>
2026-03-20 22:45:40 +0530

    fix: random quote every run

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from post import post_to_linkedin
 from datetime import datetime
 import json 
 import os 
+import random
 
 logging.basicConfig(
   level=logging.INFO,
@@ -21,27 +22,18 @@ INDEX_FILE = "index.txt"
 
 
 def get_topic():
-    if os.path.exists(INDEX_FILE): 
-       with open(INDEX_FILE,"r") as f : 
-          index = int(f.read().strip())
-    else : 
-       index = 0 
+    with open(QUOTES_FILE, "r", encoding="utf-8") as f:
+        quotes = json.load(f)
     
-    with open(QUOTES_FILE,"r",encoding="utf-8") as f :
-       quotes = json.load(f)
-
-    if index > len(quotes): 
-       index = 0
-       logger.warning("All quotes are used ! Restarting from beginning")
-
-    item = quotes[index]
-    quote = item.get("quote","").strip()
-    author = item.get("author","Unknown").strip()
-
-    with open(INDEX_FILE,"w") as f: 
-       f.write(str(index+1))
-
-    logger.info(f"Quote #{index} selected. Remaining: {len(quotes) - index - 1}")
+  
+    seed = int(datetime.now().timestamp())
+    random.seed(seed)
+    
+    item = random.choice(quotes)
+    quote = item.get("quote", "").strip()
+    author = item.get("author", "Unknown").strip()
+    
+    logger.info(f"Quote selected: {quote[:40]}...")
     return f"{quote} — {author}"
 
 def run():
@@ -73,3 +65,4 @@ def run():
 
 if __name__ == "__main__":
   run()
+

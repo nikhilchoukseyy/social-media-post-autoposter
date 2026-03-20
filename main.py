1bf9441f998422d07bc421021c604e90f2e46a9c
nikhil chouksey <nikhilranger18@gmail.com>
2026-03-20 19:18:09 +0530

    Quotes json file added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 from generate import generate_caption, generate_image
 from post import post_to_linkedin
 from datetime import datetime
+import json 
+import os 
 
 logging.basicConfig(
   level=logging.INFO,
@@ -14,24 +16,33 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def get_topic():
-    from generate import client
-    
-    response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[
-            {
-                "role": "user",
-                "content": """Give me ONE unique motivational quote for a LinkedIn post today.
-                - Should be fresh and specific , length should be 25-30 words"""
-            }
-        ]
-    )
-    
-    topic = response.choices[0].message.content.strip()
-    logger.info(f"Topic selected: {topic}")
-    return topic 
+QUOTES_FILE = "quotes.json"
+INDEX_FILE = "index.txt"
 
+
+def get_topic():
+    if os.path.exists(INDEX_FILE): 
+       with open(INDEX_FILE,"r") as f : 
+          index = int(f.read().strip())
+    else : 
+       index = 0 
+    
+    with open(QUOTES_FILE,"r",encoding="utf-8") as f :
+       quotes = json.load(f)
+
+    if index > len(quotes): 
+       index = 0
+       logger.warning("All quotes are used ! Restarting from beginning")
+
+    item = quotes[index]
+    quote = item.get("quote","").strip()
+    author = item.get("author","Unknown").strip()
+
+    with open(INDEX_FILE,"w") as f: 
+       f.write(str(index+1))
+
+    logger.info(f"Quote #{index} selected. Remaining: {len(quotes) - index - 1}")
+    return f"{quote} — {author}"
 
 def run():
     logger.info("=== AutoPoster Started ===")
